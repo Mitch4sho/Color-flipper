0ea0466c40ff7fa540f38a7d209980072e15b5a4
Mitch4sho <mitchelloliver1991@gmail.com>
2020-09-07 07:31:31 -0700

    Finished the functionaltiy for both Simple and Hex color flipper

--- hex.js
+++ hex.js
@@ -1,8 +1,12 @@
-const hexCodeArray = ['#BEE6CE', '#BCFFDB', '#8DFFCD', '#68D89B', '#4F9D69'];
+const hexCodeArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', "B", 'C', 'D', 'E', 'F'];
 
 
 function colorChanger() {
-    var rndNum = Math.floor(Math.random() * hexCodeArray.length);
-    document.getElementById('code').textContent = hexCodeArray[rndNum];
-    document.querySelector('.mainSection').style.background = hexCodeArray[rndNum];
+    let hexColor = '#';
+    for (let i = 0; i < 6; i++) {
+        var rndNum = Math.floor(Math.random() * hexCodeArray.length);
+        hexColor += hexCodeArray[rndNum];
+    }
+    document.getElementById('code').textContent = hexColor;
+    document.querySelector('.mainSection').style.background = hexColor;
 }
